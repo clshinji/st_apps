1b12eaa975c4011a13dbc541a09cb5eee070d028
clshinji <clshinji@gmail.com>
2023-07-25 00:01:24 +0900

    Delete road_yml.py

--- road_yml.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-class yml_list:
-    def __init__(self):
-        with open('config.yml', 'r') as file:
-            data = yaml.safe_load(file)
-        
-        self.column_names_pg1 = data['column_names_pg1']
-        self.column_names_pg2 = data['column_names_pg2']
